d6c6773fbd37c232487e7e670540c8b843746989
vikhulei <vikhulei@gmail.com>
2021-06-19 22:25:46 +0300

    adding multiple users added

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -5,44 +5,46 @@ class FetchData extends React.Component {
         super(props);
         this.state = {
             fetchedData: [],
-            fullName: [],
+            userInformation: [],
+            usersInfos: [],
         }
         this.fetchData = this.fetchData.bind(this)
+        this.loadData = this.loadData.bind(this)
         this.showUser = this.showUser.bind(this)
+        this.addUsers = this.addUsers.bind(this)
         this.getFullUserInfo = this.getFullUserInfo.bind(this)
         this.getUserPicture = this.getUserPicture.bind(this)
     }
 
-    componentDidMount() {
-        fetch("https://randomuser.me/api")  
+
+    fetchData() {
+        return fetch("https://randomuser.me/api")  
         .then(response => response.json()
         )
-        .then(data =>                
-                this.setState({fetchedData: data.results})
-                )
         .catch(err => {
             console.error(err)
         })
     }
 
-    fetchData() {
-        fetch("https://randomuser.me/api")  
-        .then(response => response.json()
-        )
-        .then(data => 
-            this.setState({fetchedData: 
-                data.results})
-        )
-        .catch(err => {
-            console.error(err)
-        })
+    loadData() {
+       this.fetchData()
+        .then(data => data.results)
+        .then(data => this.setState({fetchedData: data}))
     }
 
     showUser() {
-        fetch("https://randomuser.me/api")
-        .then(response => response.json())
-        .then(data => this.setState({fullName: data.results})
-         )
+        this.fetchData()
+        .then(data => {this.setState({userInformation: data.results})
+        })
+    }
+
+    addUsers() {
+        this.fetchData()
+        .then(data => data.results)
+        .then(data => 
+            {
+                this.setState({usersInfos: [...this.state.usersInfos, data[0]] })
+            })
     }
 
     getFullUserInfo(userInfo) {
@@ -55,19 +57,27 @@ class FetchData extends React.Component {
         return large
     }
 
-
     render () {
         return <div>
-            <button onClick={this.fetchData}>Load Data</button>
+            <button onClick={this.loadData}>Load Data</button>
             <button onClick={this.showUser}>Show User</button>
+            <button onClick={this.addUsers}>Add Users</button>
             <div>
                 {this.state.fetchedData.map((item, idx) => (
                     <p key={idx}>{JSON.stringify(item)}</p>
                 ))}
             </div>
             <div>
-                {this.state.fullName.map((userInfo, idx) => (
+                {this.state.userInformation.map((userInfo, idx) => (
                     <div key={idx}>
+                        <p>{this.getFullUserInfo(userInfo)}</p>
+                        <img src={this.getUserPicture(userInfo)}/>
+                    </div>
+                ))}
+            </div>
+            <div style={{display: "flex", flexWrap: "wrap"}}>
+                {this.state.usersInfos.map((userInfo, idx) => (
+                    <div key={idx} style={{marginRight: "10px"}}>
                         <p>{this.getFullUserInfo(userInfo)}</p>
                         <img src={this.getUserPicture(userInfo)}/>
                     </div>
