96abbdee29856da6fe5349c6ce3512bc7125d9ca
viktor <vikhulei@gmail.com>
2021-06-30 20:53:53 +0300

    fetch on the screen

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,14 +1,20 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 
 const Fetch = () => {
 
 const [fetchedData, setFetchedData] = useState()
 
-
+useEffect(() => {
+    fetch("https://randomuser.me/api")
+    .then(data => data.json())
+    .then(data => data.results.map(val => val))
+    .then(data => setFetchedData(JSON.stringify(data)))
+}, [])
 
     return <div>
         <h1>Hello from Fetch</h1>
+        <p>{fetchedData}</p>
     </div>
 }
 
