2194a5c5174de627c2c076efabf0be5cfc287dbd
viktor <vikhulei@gmail.com>
2021-06-30 15:24:20 +0300

    Blank copy created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Counter from "./components/Counter"
-import FetchData from "./components/Fetch"
 
 
 const App = () => {
 
 return (
   <div className="App">
-    <Counter />
-    <FetchData />
+
   </div>
 )
 
