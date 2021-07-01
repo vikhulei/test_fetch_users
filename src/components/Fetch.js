0f9b73a15b458992a8c19bd02134bf5da289d057
viktor <vikhulei@gmail.com>
2021-07-01 14:56:04 +0300

    working on map in return

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -4,18 +4,27 @@ import axios from "axios"
 
 const Fetch = () => {
 
-const [fetchedData, setFetchedData] = useState()
+const [fetchedData, setFetchedData] = useState([])
+const [userInf, setUserInf] = useState([])
+const [urlPic, setUrlPic] = useState("")
 
 useEffect(() => {
     axios.get("https://randomuser.me/api")
     .then(data => data.data.results)
-    .then(data => data.map(val => val))
     .then(data => setFetchedData(JSON.stringify(data)))
-}, [])
+}, []) 
+
+const showUser = () => {
+    const title = JSON.parse(fetchedData)
+    console.log(title[0].name.title)
+    setUserInf(title)
+}
 
     return <div>
         <h1>Hello from Fetch</h1>
-         <p>{fetchedData}</p> 
+         <p>{fetchedData}</p>
+         <button onClick={showUser}>Show User</button>
+         <div>{userInf}</div>
     </div>
 }
 
